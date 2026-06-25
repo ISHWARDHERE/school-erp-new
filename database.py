31430bb66eedf84e69005c96efdc0d988f00c1fc
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-25 11:34:14 -0400

    Fixed database syntax

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 import mysql.connector
 
 def connect_db():
-    conn = mysql.connector.connect(
-        host="proxy.rlwy.net",
-        user="root",
-        password="rtWhYEPElwzIYDTTFkYgUZxxAcBZekLq",
-        database="railway",
-        port=57454
-    )
-    return conn
+    try:
+        conn = mysql.connector.connect(
+            host="proxy.rlwy.net",
+            user="root",
+            password="YOUR_RAILWAY_PASSWORD",
+            database="railway",
+            port=57454
+        )
+        return conn
 
     except Exception as e:
         print("Database Error:", e)
