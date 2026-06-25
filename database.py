23243994f8b04234e5bb9c0179b28415c7c34442
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-25 10:23:51 -0400

    Railway DB Connected

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ import mysql.connector
 def connect_db():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="schooladmin",
-            password="admin123",
-            database="school_db",
-            port=3306
+           host="MYSQLHOST",
+           user="MYSQLUSER",
+           password="MYSQLPASSWORD",
+           database="MYSQLDATABASE",
+           port=MYSQLPORT
         )
 
         if conn.is_connected():
