2a67824c700e9b5625edd4b920af601c834e6250
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-26 02:35:45 -0400

    Fixed secret key

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
+import os
 import mysql.connector
 
 def connect_db():
     try:
         conn = mysql.connector.connect(
-            host="reseau.proxy.rlwy.net",
-            user="root",
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            database="railway",
-            port=57454
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))
         )
         return conn
 
