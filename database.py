d67e209f746cdf16d78b0aadd3bbcefd57b622b8
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-26 02:56:09 -0400

    Direct DB test

--- database.py
+++ database.py
@@ -1,16 +1,13 @@
-import os
 import mysql.connector
 
 def connect_db():
     try:
-        db_port = int(os.getenv("DB_PORT"))
-
         conn = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
-            database=os.getenv("DB_NAME"),
-            port=db_port
+            host="reseau.proxy.rlwy.net",
+            user="root",
+            password="rtWhYEPElwzIYDTTFkYgUZxxAcBZekLq",
+            database="railway",
+            port=57454
         )
 
         print("Database Connected")
