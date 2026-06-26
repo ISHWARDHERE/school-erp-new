9f5aefa5d3893d5b2f8b08ae73c0b49a53113da7
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-26 01:12:14 -0400

    Fixed Railway public DB

--- database.py
+++ database.py
@@ -1,28 +1,16 @@
-import os
 import mysql.connector
 
 def connect_db():
     try:
-        host = os.getenv("DB_HOST")
-        user = os.getenv("DB_USER")
-        password = os.getenv("DB_PASS")
-        database = os.getenv("DB_NAME")
-        port = int(os.getenv("DB_PORT"))
-
-        print(host, user, database, port)
-
         conn = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            port=port,
-            connection_timeout=10
+            host="reseau.proxy.rlwy.net",
+            user="root",
+            password="rtWhYEPElwzIYDTTFkYgUZxxAcBZekLq",
+            database="railway",
+            port=57454
         )
-
-        print("Database Connected")
         return conn
 
     except Exception as e:
-        print("Database Error:", str(e))
+        print("Database Error:", e)
         return None
