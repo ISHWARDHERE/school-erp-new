fb99259db9fd4eaa68fd2e266caa1c85dec6673c
Jagdamba Enterprises <jagadambanetcafe@gmail.com>
2026-06-26 02:52:20 -0400

    Final DB port fix

--- database.py
+++ database.py
@@ -3,13 +3,16 @@ import mysql.connector
 
 def connect_db():
     try:
+        db_port = int(os.getenv("DB_PORT"))
+
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
             database=os.getenv("DB_NAME"),
-            port=int(os.getenv("DB_PORT"))
+            port=db_port
         )
+
         print("Database Connected")
         return conn
 
